d27680105a2c0e3504ae67b66f8df718f147d644
Mohit Bhandari <mohitbhandari6112@gmail.com>
2023-04-07 23:08:47 +0545

    some update in the code

--- src/js/controller.js
+++ src/js/controller.js
@@ -128,9 +128,6 @@ const controlAddRecipe = async function (newRecipe) {
     console.error('😵', err);
   }
 };
-const newFeature = function () {
-  console.log('welcome back man');
-};
 
 const init = function () {
   bookmarksView.addHandlerRender(controlAddBookmarks);
@@ -140,6 +137,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
